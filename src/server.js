2a593be0f21ea70cc6c8cbc3c6031fa5ed1dacc9
Oleksandr Yahodkin <0506aleksandr@gmail.com>
2026-09-14 14:12:41 +0300

    port 3000

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
-import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 // Middleware для парсингу JSON
 app.use(express.json());
 app.use(cors()); // Дозволяє запити з будь-яких джерел
 app.use(
   pino({
-    level: "info",
+    level: 'info',
     transport: {
-      target: "pino-pretty",
+      target: 'pino-pretty',
       options: {
         colorize: true,
-        translateTime: "HH:MM:ss",
-        ignore: "pid,hostname",
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
         messageFormat:
-          "{req.method} {req.url} {res.statusCode} - {responseTime}ms",
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
@@ -28,14 +28,14 @@ app.use(
 );
 
 // Список усіх нотаток
-app.get("/notes", (req, res) => {
+app.get('/notes', (req, res) => {
   res.status(200).json({
-    message: "Retrieved all notes",
+    message: 'Retrieved all notes',
   });
 });
 
 // Конкретна нотатка за id
-app.get("/notes/:noteId", (req, res) => {
+app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({
     message: `Retrieved note with ID: ${noteId}`,
@@ -43,21 +43,21 @@ app.get("/notes/:noteId", (req, res) => {
 });
 
 // Маршрут для тестування middleware помилки
-app.get("/test-error", (req, res) => {
+app.get('/test-error', (req, res) => {
   // Штучна помилка для прикладу
-  throw new Error("Simulated server error");
+  throw new Error('Simulated server error');
 });
 
 // Middleware 404 (після всіх маршрутів)
 app.use((req, res) => {
   res.status(404).json({
-    message: "Route not found",
+    message: 'Route not found',
   });
 });
 
 // Middleware для обробки помилок
 app.use((err, req, res, next) => {
-  console.error("Error:", err.message);
+  console.error('Error:', err.message);
   res.status(500).json({
     message: err.message,
   });
