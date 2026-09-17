ddca19068486406f93a4d55673bbe843c41954bf
Oleksandr Yahodkin <0506aleksandr@gmail.com>
2026-09-17 21:43:40 +0300

    fix: configure SMTP transport

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,46 +1,19 @@
 import nodemailer from 'nodemailer';
- import { env } from './env.js';
-// import axios from 'axios';
+import { env } from './env.js';
 
 const transporter = nodemailer.createTransport({
   host: env('SMTP_HOST'),
-  port: env('SMTP_PORT'),
-  secure: true,
+  port: Number(env('SMTP_PORT')),
+  secure: false,
   auth: {
     user: env('SMTP_USER'),
     pass: env('SMTP_PASSWORD'),
   },
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error('SMTP connection failed:', error);
-  } else {
-    console.log('Server is ready to send emails');
-  }
-});
-
-export const sendMail = async (options) => {
-  return await transporter.sendMail(options);
+export const sendEmail = async (options) => {
+  return await transporter.sendMail({
+    from: env('SMTP_FROM'),
+    ...options,
+  });
 };
-
-// const API_KEY = env('MAILERSEND_API_KEY');
-
-// export const sendMail = async ({ to, subject, html, text }) => {
-//   return await axios.post(
-//     env('MAILERSEND_API_URL'),
-//     {
-//       from: { email: env('SMTP_FROM') },
-//       to: [{ email: to }],
-//       subject: subject,
-//       text: text,
-//       html: html,
-//     },
-//     {
-//       headers: {
-//         Authorization: `Bearer ${API_KEY}`,
-//         'Content-Type': 'application/json',
-//       },
-//     },
-//   );
-// };
